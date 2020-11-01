db7637bde9fba8e28ba9cc0d9a775675b459f78d
KarryCMT <1570606465@qq.com>
2020-11-01 11:52:52 +0800

    commit code

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="com-container">
+  <div class="com-container" @dblclick="revertMap">
     <div class="com-chart" ref="map_ref"></div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { getProvinceMapInfo } from '../utils/map_utils'
 export default {
   name: 'Map',
   components: {},
@@ -15,14 +17,12 @@ export default {
       allData: null,
       showChoice: false,
       choiceType: 'map',
-      titleFontSize: 0
+      titleFontSize: 0,
+      mapData: {}
     }
   },
   created() {},
   watch: {},
-  destroyed() {
-    window.removeEventListener('resize', this.screenAdapter)
-  },
   computed: {},
   mounted() {
     this.initChart()
@@ -30,93 +30,125 @@ export default {
     window.addEventListener('resize', this.screenAdapter)
     this.screenAdapter()
   },
-
+  destroyed() {
+    window.removeEventListener('resize', this.screenAdapter)
+  },
   methods: {
     // !初始化echarts对象
-    initChart() {
+    async initChart() {
       this.chartInstance = this.$echarts.init(this.$refs.map_ref, 'chalk')
+      const { data: res } = await axios.get(
+        'http://localhost:9090/static/map/china.json'
+      )
+      this.$echarts.registerMap('china', res)
       const initOption = {
-        grid: {
-          top: '35%',
-          left: '3%',
-          right: '4%',
-          bottom: '1%',
-          containLabel: true
+        title: {
+          text: '▎商家分布',
+          left: 20,
+          top: 20
         },
-        xAxis: {
-          type: 'category',
-          boundaryGap: false
-        },
-        yAxis: {
-          type: 'value'
-        },
-        tooltip: {
-          trigger: 'axis'
+        geo: {
+          type: 'map',
+          map: 'china',
+          top: '5%',
+          bottom: '5%',
+          itemStyle: {
+            areaColor: '#2E72BF',
+            borderColor: '#333'
+          }
         },
         legend: {
-          left: 20,
-          top: '15%',
-          icon: 'circle'
-        },
-        series: [
-          {
-            type: 'line'
-          }
-        ]
+          left: '5%',
+          bottom: '5%',
+          orient: 'vertical'
+        }
       }
 
       this.chartInstance.setOption(initOption)
+      //! 省份地图数据
+      this.chartInstance.on('click', async arg => {
+        const provinceInfo = getProvinceMapInfo(arg.name)
+        if (!this.mapData[provinceInfo.key]) {
+          const { data: ret } = await axios.get(
+            'http://localhost:9090' + provinceInfo.path
+          )
+          this.mapData[provinceInfo.key] = ret
+          this.$echarts.registerMap(provinceInfo.key, ret)
+        }
+
+        const changeOption = {
+          geo: {
+            type: 'map',
+            map: provinceInfo.key,
+            top: '5%',
+            bottom: '5%',
+            itemStyle: {
+              areaColor: '#2E72BF',
+              borderColor: '#333'
+            }
+          }
+        }
+        this.chartInstance.setOption(changeOption)
+      })
     },
     // !获取服务器数据
     async getData() {
       const { data: res } = await this.$api.get('map')
       this.allData = res
+      console.log(this.allData)
       this.updateChart()
     },
     // !更新图表
     updateChart() {
-      const dataOption = {}
+      const legendArr = this.allData.map(item => {
+        return item.name
+      })
+      const seriesArr = this.allData.map(item => {
+        return {
+          rippleEffect: {
+            scale: 5,
+            brushType: 'stroke'
+          },
+          type: 'effectScatter',
+          name: item.name,
+          data: item.children,
+          coordinateSystem: 'geo'
+        }
+      })
+      const dataOption = {
+        legend: { data: legendArr },
+        series: seriesArr
+      }
       this.chartInstance.setOption(dataOption)
     },
+    //! 回到中国地图
+    revertMap() {
+      const revertOption = {
+        geo: {
+          map: 'china'
+        }
+      }
 
+      this.chartInstance.setOption(revertOption)
+    },
     // !屏幕适配
     screenAdapter() {
-      this.titleFontSize = (this.$refs.trend_ref.offsetWidth / 100) * 3.6
+      this.titleFontSize = (this.$refs.map_ref.offsetWidth / 100) * 3.6
       //分辨率大小相关的配置项
       const adapterOption = {
         legend: {
-          itemWidth: this.titleFontSize,
-          itemHeight: this.titleFontSize,
-          itemGap: this.titleFontSize,
+          itemWidth: this.titleFontSize / 2,
+          itemHeight: this.titleFontSize / 2,
+          itemGap: this.titleFontSize / 2,
           textStyle: {
             fontSize: this.titleFontSize / 2
           }
         },
-        // title: {
-        //   textStyle: {
-        //     fontSize: this.titleFontSize
-        //   }
-        // },
-        // tooltip: {
-        //   axisPointer: {
-        //     lineStyle: {
-        //       width: this.titleFontSize
-        //     }
-        //   }
-        // },
-        series: [
-          {
-            barWidth: this.titleFontSize,
-            itemStyle: {
-              barBorderRadius: [
-                0,
-                this.titleFontSize / 2,
-                this.titleFontSize / 2,
-                0
-              ]
-            }
+        title: {
+          textStyle: {
+            fontSize: this.titleFontSize
           }
-        ]
+        }
       }
 
       this.chartInstance.setOption(adapterOption)
